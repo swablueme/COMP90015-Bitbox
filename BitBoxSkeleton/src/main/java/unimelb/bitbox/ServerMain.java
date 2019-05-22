b5cf587bbbc264656db4164ebce31e734c9efbf8
swablueme <swablueme@gmail.com>
2019-05-22 14:22:11 +1000

    adding UDP

--- BitBoxSkeleton/src/main/java/unimelb/bitbox/ServerMain.java
+++ BitBoxSkeleton/src/main/java/unimelb/bitbox/ServerMain.java
@@ -17,74 +17,42 @@ import unimelb.bitbox.util.Document;
 import java.util.concurrent.*;
 
 public class ServerMain implements FileSystemObserver {
-    
+
     private static Logger log = Logger.getLogger(ServerMain.class.getName());
     protected FileSystemManager fileSystemManager;
-    
+
     public static BlockingQueue<Document> messageQueue = new LinkedBlockingQueue<Document>();
-    
+
     public ServerMain() throws NumberFormatException, IOException, NoSuchAlgorithmException {
         fileSystemManager = new FileSystemManager(Configuration.getConfigurationValue("path"), this);
         new actOnMessages(fileSystemManager);
     }
-    
+
     @Override
     public void processFileSystemEvent(FileSystemEvent fileSystemEvent) {
         Logger log = Logger.getLogger(FileSystemManager.class.getName());
         //converts a fileSystemEvent into json ready to send
         String myjson = jsonMarshaller.fileEventToJson(fileSystemEvent);
-        ArrayList<clientSocket> peeroutputstreams = peerList.getPeerList();
-        if (!(peeroutputstreams.isEmpty())) {
-            for (clientSocket myclient : peeroutputstreams) {
-                //sends a fileSystem event that has been triggered into the output stream
-                myclient.write(myjson);
+        if (Peer.mode.equals("tcp")) {
+            ArrayList<clientSocket> peeroutputstreams = peerList.getPeerList();
+            if (!(peeroutputstreams.isEmpty())) {
+                for (clientSocket myclient : peeroutputstreams) {
+                    //sends a fileSystem event that has been triggered into the output stream
+                    myclient.write(myjson);
+                }
+            }
+
+        } else {
+            System.out.println("writing event");
+            ArrayList<udpSocket> peers= udpPeerList.getPeerList();
+            System.out.println("peerlist: "+peers);
+            if (!(peers.isEmpty())) {
+                for (udpSocket myclient : peers) {
+                    System.out.println("we are intending to write to: "+myclient.toHostport());
+                    myclient.write(myjson);
+                }
             }
         }
-
-        //prettyPrinter.print(myjson);
-        /*
-        if (fileSystemEvent.event == FileSystemManager.EVENT.FILE_CREATE) {
-            log.info("processFileSystemEvent OCCURING FOR EVENT: " + fileSystemEvent.fileDescriptor.md5);
-            String mymessage=jsonMarshaller.createFILE_CREATE_REQUEST(
-                                            fileSystemEvent.fileDescriptor.toDoc(), 
-                                            fileSystemEvent.name);
-            prettyPrinter.print(mymessage);
-         */
- /*THIS CREATES A FILE DUPLICATE MADE BY CREATING A BYTE64 buffer and encoding then deencoding it
- //some of the bits of code here were written up more neatly in the base64Handler
-            
-            try {
-                ByteBuffer test = fileSystemManager.readFile(fileSystemEvent.fileDescriptor.md5, 0, 
-                        fileSystemEvent.fileDescriptor.fileSize);
-                System.out.println("creates an example bytebuffer");
-                //reads a bytebuffer into string after getting the bytebuffer as a bytearray
-                String encoded = Base64.getEncoder().encodeToString(test.array());
-                //now we convert the bytebuffer back into a byte array
-                byte[] barr = Base64.getDecoder().decode(encoded);
-                //now we turn a bytearray into a bytebuffer
-                ByteBuffer buf = ByteBuffer.wrap(barr);
-                //we check if the path is alright and if the filename doesn't already exist
-                if ((fileSystemManager.isSafePathName(fileSystemEvent.path+"2") == true)
-                        && (fileSystemManager.fileNameExists(fileSystemEvent.pathName+"2") == false)) {
-                    //we make a fileloader to hold what we are going to write in
-                    fileSystemManager.createFileLoader(fileSystemEvent.name + "2", fileSystemEvent.fileDescriptor.md5, 0, 
-                                    fileSystemEvent.fileDescriptor.lastModified);
-                    System.out.println("MADE FILE LOADER");
-                    //we write our file
-                    fileSystemManager.writeFile(fileSystemEvent.name + "2", buf, 0);
-
-                    //we check if it has been written properly
-        
-                    fileSystemManager.checkWriteComplete(fileSystemEvent.name + "2");
-                    System.out.println("CHECKING COMPLETE");
-
-                }
-
-            } catch (Exception e) {
-                exceptionHandler.handleException(e);
-            }
-         */
     }
-    //System.out.println(fileSystemEvent.fileDescriptor.lastModified);
-    // TODO: process events
+
 }
--- BitBoxSkeleton/src/main/java/unimelb/bitbox/baseSocket.java
+++ BitBoxSkeleton/src/main/java/unimelb/bitbox/baseSocket.java
@@ -4,31 +4,58 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.logging.Logger;
+import unimelb.bitbox.util.Document;
 
 public class baseSocket<T> {
+
     //todo: make getters rather than using public variables
     public String type = null;
     protected HashMap bufferedStreams = null;
     private static Logger log = Logger.getLogger(Peer.class.getName());
+    protected static int clientCount = 0;
+    protected Integer connRequestServerPort = null;
+    protected String connRequestHost = null;
     //can hold a socket
     AutoCloseable socket = null;
 
-    baseSocket(String type) {     
+    baseSocket(String type) {
         //saves what type of socket it is if read from config or made from server accepts
         this.type = type;
         //log.info("Initialising base socket");
     }
+
+    public void setCONNECTION_REQUESTdetails(Document handshakeDetails) {
+        if (type.equals("client from config")) {
+            clientCount++;
+        }
+        Document hostport = (Document) handshakeDetails.get("hostPort");
+        this.connRequestServerPort = Integer.parseInt(hostport.get("port").toString());
+        this.connRequestHost = (String) ((Document) handshakeDetails.get("hostPort")).getString("host");
+
+    }
+
+    public Integer getconnRequestServerPort() {
+        return this.connRequestServerPort;
+    }
+
+    public String connRequestHost() {
+        return this.connRequestHost;
+    }
+
     //returns the inputstream of the socket
     public T getBufferedInputStream() {
         return (T) bufferedStreams.get("inputStream");
     }
+
     //returns the output stream of the socket
     public T getBufferedOutputStream() {
         return (T) bufferedStreams.get("outputStream");
     }
+
     //makes a map storing the buffered streams
     public <T> HashMap<String, T> createBufferedStreams(Socket socket) {
         BufferedReader in = null;
@@ -44,6 +71,7 @@ public class baseSocket<T> {
         bufferedStreams.put("outputStream", (T) out);
         return bufferedStreams;
     }
+
     //close method that should be able to close a ServerSocket or a regular client Socket
     public <T extends AutoCloseable> void close() {
         System.out.println("trying to close socket");
@@ -56,6 +84,17 @@ public class baseSocket<T> {
                 log.info("Closing socket");
             }
         }
+    }
+
+    public <T extends AutoCloseable> void write(String message) {
+
+    }
+
+    public <T extends AutoCloseable> String toHostport() {
+        return "";
+    }
+
+    public <T extends AutoCloseable> void setHostPort(InetAddress host, Integer port) {
     }
 
 }
