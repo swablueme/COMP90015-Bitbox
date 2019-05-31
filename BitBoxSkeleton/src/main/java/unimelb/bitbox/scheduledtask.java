84ebe6bf3587e83e9740e3ad326a9e88ebc600b9
swablueme <swablueme@gmail.com>
2019-05-31 15:51:16 +1000

    added remove peers function to peerlists -- TODO: fix scheduler

--- BitBoxSkeleton/src/main/java/unimelb/bitbox/scheduledtask.java
+++ BitBoxSkeleton/src/main/java/unimelb/bitbox/scheduledtask.java
@@ -10,36 +10,37 @@ import java.util.ArrayList;
 import unimelb.bitbox.util.Document;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 public class scheduledtask implements Runnable {
 
     public static DatagramSocket socket = null;
 
     scheduledtask(udpSocket toWriteTo) {
-        System.out.println("HERE'S THE SOCKET");
-        System.out.println(toWriteTo);
         this.socket = udpSocket.clientSock;
         run();
     }
 
     public void run() {
-        System.out.println("OOOOHHHH RUNNNING");
+        System.out.println("thread name: "+Thread.currentThread().getName());
         LocalDateTime dateTime = LocalDateTime.now();
-        LocalDateTime dateTimein2 = dateTime.plus(Duration.of(10, ChronoUnit.SECONDS));
+        LocalDateTime dateTimein2 = dateTime.plus(Duration.of(Peer.timeout, ChronoUnit.SECONDS));
         while (true) {
             LocalDateTime current = LocalDateTime.now();
             HashMap<String, ArrayList<Document>> toSend = determineNeedsResending.getList();
             HashMap<Document, ArrayList<Object>> toSendDates = determineNeedsResending.getDateMap();
+            System.out.println("");
             for (String user : toSend.keySet()) {
                 ArrayList<Document> userMessages = toSend.get(user);
                 for (Document message : userMessages) {
                     ArrayList<Object> messageDateAndTries = toSendDates.get(message);
-                    LocalDateTime toSendTime = (LocalDateTime) messageDateAndTries.get(0);
-                    if (current.isAfter(toSendTime) && (Integer) messageDateAndTries.get(1)<=3) {
-                        //System.out.println("ooooh out of DATEEEE: " + toSendTime + "message: " + message);
+                    LocalDateTime toSendTime = (LocalDateTime) messageDateAndTries.get(0);    
+                    if (current.isAfter(toSendTime) && (Integer) messageDateAndTries.get(1)<=Peer.retries) {
+                        System.out.println("user is currently: "+user);
+                        System.out.println("message is currently: "+message);
+                        System.out.println(messageDateAndTries);
                         String toSendMessage = message.toJson();
                         byte[] buffer = toSendMessage.getBytes();
-                        //if (super.connRequestServerPort == null && super.connRequestHost == null) {
                         try {
                             String mystring = user.split(":")[0];
                             String newstring = mystring.substring(1, mystring.length());
@@ -49,16 +50,16 @@ public class scheduledtask implements Runnable {
                             messageDateAndTries.set(0, ((LocalDateTime) messageDateAndTries.get(0)).plus(Duration.of(10, ChronoUnit.SECONDS)));
                             
                             this.socket.send(packet);
+                            System.out.println("SENNTTT");
                             prettyPrinter.print(toSendMessage);
-                            break;
                         } catch (Exception e) {
                             exceptionHandler.handleException(e);
                         }
-                        break;
                     }
 
                 }
             }
+            
 
         }
 
